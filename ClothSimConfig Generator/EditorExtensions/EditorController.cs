306077b385fcdcf0b5b1cdc7824500e9966e0584
Sam Mair <sam.mair@bigant.com>
2019-09-16 17:05:46 +1000

    Added EditorController to stop Unity editor from jumping to game window.

---
+++ ClothSimConfig Generator/EditorExtensions/EditorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+
+public class EditorController : MonoBehaviour
+{
+    public bool useSceneView = false;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (useSceneView)
+        {
+            UnityEditor.SceneView.FocusWindowIfItsOpen<UnityEditor.SceneView>();
+        }
+    }
+}
+#endif
